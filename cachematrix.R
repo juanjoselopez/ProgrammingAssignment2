71c8b7c1311239b0555750299dc876568ea595c0
juanjoselopez <ljuanjos@gmail.com>
2020-11-11 15:59:46 -0300

    Generando commit de la data para la evaluación en la plataforma github de la tarea por encargo de la semana 3

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,49 @@
 
 ## Write a short comment describing this function
 
+#La siguiente función almacenará la inversa de una matriz
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # Primero se crea la variable caché vacía
+    inversa <- NULL
+    
+    #En el siguiente código se establece el valor de la matriz
+    set <- function(y){
+        #El operador <<- permite a "set" instanciar x fuera del entorno de la función
+        x <<- y
+        inversa <<- NULL
+    }
+    
+    #Obtener el valor de la matriz
+    get <- function() x #retorna x
+    
+    #Obtener el valor de la inversa y almacenarla en "inversa"
+    setinversa <- function(inv) inversa <<- inv
+    
+    #Recuperar el valor de la inversa
+    getinversa <- function() inversa 
+    
+    #Enlistar las funciones para poder llamarlas luego
+    list(set = set, get = get,
+         setinversa = setinversa,
+         getinversa = getinversa)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#La siguiente función obtendrá la inversa en caché
+cacheInversa <- function(x, ...){
+    
+    #Obtener el valor retornado de la inversa
+    inversa <- x$getinversa()
+    
+    #Condición de error
+    #Si el valor del caché NO es nulo, obtener caché...
+    if(!is.null(inversa)){
+        message("Obteniendo Data en Cache")
+        return(inversa)
+    }
+    #...Si ES nulo, entonces obtener la inversa (con solve()) y almacenarla en caché
+    valor <- x$get()
+    inversa <- solve(valor,...)
+    x$setinversa(inversa)
+    inversa #retornar la inversa
 }
